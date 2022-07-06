06c937171054b706d711943ddfa9501bf0ead5af
MarinoSigIT <marinosignorini.it@gmail.com>
2022-07-06 12:44:21 +0000

    todo list with react

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,67 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+const [arrayoflist, SetArrayoflist] = useState([]);
+const [inputvalue, SetInputvalue] = useState();
+
+const catchInputValue = (event) => {
+	if (event.keyCode === 13) {
+		SetInputvalue(event.target.value);
+		SetArrayoflist([...arrayoflist, event.target.value]);
+		event.target.value = "";
+	}
+};
+console.log(inputvalue)
+console.log(arrayoflist)
+
+const deleteListItem = () => {
+	return (<span>X</span>)
+}
+
+
+const listoftasks = arrayoflist?.map((task, index) => {
+
+    return (
+
+		<li key={index}
+		className="list-group-item " 
+		style={{liststyle: "none"}}
+		onMouseOver={deleteListItem}
+		>
+			{task}
+		</li> 
+
+
+)
+});
+
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid text-center">
+			<div className="row justify-content-md-center">
+				<input 
+					className="mt-5 border-0 col col-lg-4" 
+					placeholder="What needs to be done?"
+					size="50"
+					onKeyDown={catchInputValue}
+				/>
+			</div>
+
+			<div className="row justify-content-md-center">
+				<ul 
+					className="mt-5 border-0 col col-lg-4 list-group "
+					size="50"
+					
+				>
+					{listoftasks}
+				</ul>
+			</div>
+				
+				
 		</div>
 	);
 };

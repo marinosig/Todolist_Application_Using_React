779287b18d318ed9f231d80c8fb8a273da6b5a68
MarinoSigIT <marinosignorini.it@gmail.com>
2022-07-06 22:54:06 +0000

    todolist finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 
 
+
 //create your first component
 const Home = () => {
 
@@ -17,45 +18,60 @@ const catchInputValue = (event) => {
 console.log(inputvalue)
 console.log(arrayoflist)
 
-const deleteListItem = () => {
-	return (<span>X</span>)
+const deleteListItem = (indexV) => {
+	console.log("detelet", indexV);
+	<p>teste</p>
 }
+
+const removeProduct = (index) => {
+	SetArrayoflist([
+	  ...arrayoflist.slice(0, index),
+	  ...arrayoflist.slice(index + 1, arrayoflist.length)
+	]);
+  }
 
 
 const listoftasks = arrayoflist?.map((task, index) => {
 
     return (
+		
+			<li key={index}
+			className="list-group-item d-flex justify-content-between" 
+			style={{liststyle: "none", height: ""}}
+			>
+				<span className="p-2 text-left">{task}{index}</span>
+			<span 
+				className="p-2 text-danger" 
+				onClick={() => {
+					removeProduct(index)
+					// // SetArrayoflist(prev => prev.filter(arrayoflist => arrayoflist !== task ))
+				}}
+				style={{cursor: "pointer"}}> X
+			</span>
+			</li> 
 
-		<li key={index}
-		className="list-group-item " 
-		style={{liststyle: "none"}}
-		onMouseOver={deleteListItem}
-		>
-			{task}
-		</li> 
-
-
-)
+	)
 });
 
 
 
 	return (
-		<div className="container-fluid text-center">
+		<div className="container-fluid text-center"> 
+		<h1 className="bg-light text-muted mt-5">TODO LIST</h1>
 			<div className="row justify-content-md-center">
 				<input 
-					className="mt-5 border-0 col col-lg-4" 
-					placeholder="What needs to be done?"
-					size="50"
+					className="mt-5 border-1 col col-lg-6" 
+					placeholder="Type here what needs to be done?"
+					size="100"
 					onKeyDown={catchInputValue}
+					maxLength={50}
 				/>
 			</div>
 
 			<div className="row justify-content-md-center">
 				<ul 
-					className="mt-5 border-0 col col-lg-4 list-group "
-					size="50"
-					
+					className="mt-5 border-0 col col-lg-6 list-group d-flex"
+					size="100"				
 				>
 					{listoftasks}
 				</ul>
